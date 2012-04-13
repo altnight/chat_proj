612d047b9337cb6942576a2821ec5fca8fb1470c
atasatamatara <atasatamatara@gmail.com>
2012-04-13 18:13:27 +0900

    一旦バリデーションをオフに。countがうまくいかない…

--- chat_app/routes/index.js
+++ chat_app/routes/index.js
@@ -2,6 +2,7 @@
 /*
  * GET home page.
  */
+var redis = require('redis').createClient();
 
 exports.index = function(req, res){
   res.render('index', { title: "Welcome My Chat by " + req.session.name });
@@ -12,29 +13,36 @@ exports.signup= function(req, res){
 };
 
 exports.create_signup= function(req, res){
-  if (! /[0-9a-zA-Z\-_]+/.test(req.body.name)){
-    res.send('name は 半角英数で');
-    res.redirect('/signup');
-  }
-  if (req.body.name.length < 8){
-    res.send('name は 8文字以上にしましょう');
-    res.redirect('/signup');
-  }
-  if (! /[0-9a-zA-Z\-_]+/.test(req.body.password)){
-    res.send('password は 半角英数で');
-    res.redirect('/signup');
-  }
-  if (req.body.password.length < 8){
-    res.send('password は 8文字以上にしましょう');
-    res.redirect('/signup');
-  }
-  if (req.body.password != req.body.password2) {
-    res.send('パスワード確認が間違ってるよ');
-    res.redirect('/signup');
-  }
+  //if (! /[0-9a-zA-Z\-_]+/.test(req.body.name)){
+    //res.send('name は 半角英数で');
+    //res.redirect('/signup');
+  //}
+  //if (req.body.name.length < 8){
+    //res.send('name は 8文字以上にしましょう');
+    //res.redirect('/signup');
+  //}
+  //if (! /[0-9a-zA-Z\-_]+/.test(req.body.password)){
+    //res.send('password は 半角英数で');
+    //res.redirect('/signup');
+  //}
+  //if (req.body.password.length < 8){
+    //res.send('password は 8文字以上にしましょう');
+    //res.redirect('/signup');
+  //}
+  //if (req.body.password != req.body.password2) {
+    //res.send('パスワード確認が間違ってるよ');
+    //res.redirect('/signup');
+  //}
+  var uid = 0;
+  uid++;
+  auth_JSON= { "uid" : uid,
+               "name" : req.body.name,
+               "password": req.body.password };
+
+  redis.set("auth:" + uid, JSON.stringify(auth_JSON));
+
   req.session.name = req.body.name;
   req.session.password = req.body.password;
-  req.session.password2 = req.body.password2;
   res.redirect('/');
 };
 
@@ -44,8 +52,6 @@ exports.login= function(req, res){
 
 exports.create_login= function(req, res){
   //res.render('login', { title: 'login' });
-  if (req.session.password != req.session.password2)
-    res.sender('differendt password');
 
 };
 
@@ -53,7 +59,6 @@ exports.logout= function(req, res){
   //res.render('logout', { title: 'logout' });
   delete req.session.name;
   delete req.session.password;
-  delete req.session.password2;
   req.session.name = "My Name";
   res.redirect('/');
 };
