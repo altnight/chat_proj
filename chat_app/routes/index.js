fd996ac2eefd0c05fffa840a3e88c182d12a8143
atasatamatara <atasatamatara@gmail.com>
2012-04-14 00:08:17 +0900

    なんかログインができた、ような、気が……

--- chat_app/routes/index.js
+++ chat_app/routes/index.js
@@ -5,7 +5,12 @@
 var redis = require('redis').createClient();
 
 exports.index = function(req, res){
-  res.render('index', { title: "Welcome My Chat by " + req.session.name });
+  if (req.session.name){
+    res.render('index', { title: "Welcome My Chat by " + req.session.name });
+  }
+  else {
+    res.render('index', { title: "Welcome My Chat by 増田"});
+  }
 };
 
 exports.signup= function(req, res){
@@ -33,17 +38,24 @@ exports.create_signup= function(req, res){
     //res.send('パスワード確認が間違ってるよ');
     //res.redirect('/signup');
   //}
-  var uid = 0;
-  uid++;
-  auth_JSON= { "uid" : uid,
-               "name" : req.body.name,
-               "password": req.body.password };
 
-  redis.set("auth:" + uid, JSON.stringify(auth_JSON));
-
-  req.session.name = req.body.name;
-  req.session.password = req.body.password;
-  res.redirect('/');
+  redis.get("name:" + req.body.name + ":uid", function(err, uid){
+    if (uid){
+      console.log("既に同じ名前があります");
+      res.redirect('/signup');
+    }
+    else {
+      //新規登録
+      redis.incr("uid", function(err, uid){
+        redis.set("name:" + req.body.name +":uid", uid);
+        redis.set("uid:" + uid + ":name", req.body.name);
+        redis.set("uid:" + uid + ":password", req.body.password);
+      });
+    req.session.name = req.body.name;
+    req.session.password = req.body.password;
+    res.redirect('/');
+    }
+  });
 };
 
 exports.login= function(req, res){
@@ -51,7 +63,23 @@ exports.login= function(req, res){
 };
 
 exports.create_login= function(req, res){
-  //res.render('login', { title: 'login' });
+  redis.get("name:" + req.body.name + ":uid", function(err, uid){
+    if (! uid){
+      console.log('そんな名前ないです');
+      res.redirect('/login');
+    }
+    else redis.get("uid:" + uid + ":password", function(err2, real_password){
+      if (req.body.password != real_password){
+        console.log('パスワードが違うっぽい');
+        res.redirect('/login');
+      }
+      else{
+        console.log('ログインします');
+        req.session.name = req.body.name;
+        res.redirect('/');
+      }
+    });
+  });
 
 };
 
@@ -59,7 +87,6 @@ exports.logout= function(req, res){
   //res.render('logout', { title: 'logout' });
   delete req.session.name;
   delete req.session.password;
-  req.session.name = "My Name";
   res.redirect('/');
 };
 
